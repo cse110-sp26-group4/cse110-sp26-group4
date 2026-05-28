99ef82408a51f43c6cd5ed9d0919b1b579d1ed2a
shermync <shermyn.cheah@gmail.com>
2026-05-28 13:35:51 -0700

    Edited formatting logic

--- source/commands/search.js
+++ source/commands/search.js
@@ -8,17 +8,17 @@ import { searchIssues } from "../services/issuesService.js";
 // Column widths for displaying the results
 const WIDTHS = {
   id: 5,
-  title: 30,
+  title: 20,
   status: 15,
   priority: 10,
   //assignee: 10,
-  description: 40
+  description: 60
 };
 
 /**
  * Searches for a title or description matching the command line argument
  * @param {string[]} args - The command line arguments
- * @returns {Promise<number>} The exit code: 0 is success, 1 is error, 2 is invalid input.
+ * @returns {Promise<number>} The exit code: 0 is success, 1 is error.
  */
 export async function run(args) {
     if (args.length == 0) {
@@ -86,7 +86,10 @@ function printIssueTable(issue, width) {
   const statusVal = String(issue.status || "Open");
   const priorityVal = String(issue.priority || "Low");
   //const assigneeVal = String(issue.assignee || "N/A");
-  const descVal = String(issue.desc || "N/A");
+  let descVal = String(issue.description || "N/A");
+  if (descVal.length > width.description) {
+    descVal = descVal.substring(0, width.description - 3) + "...";
+  }
 
   let row = idVal.padEnd(width.id) + " │ "
           + titleVal.padEnd(width.title) + " │ "
