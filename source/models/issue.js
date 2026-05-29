c102fc61ff29ebebf637aef525e1e4e137a20094
Efay <yifeiguy@gmail.com>
2026-05-29 14:47:09 -0700

    Added assignees to issue.js and schema.js

--- source/models/issue.js
+++ source/models/issue.js
@@ -18,16 +18,19 @@ export class Issue {
     priority    = Priority.LOW,
     tokenLimit  = null,
     description = null,
+    assignees = null,
     // Auto-generated fields
     id          = 0,
     created_at  = new Date().toISOString(),
     attempt_num = 0,
+    
   } = {}) {
     this.title       = title;
     this.status      = status;
     this.priority    = priority;
     this.tokenLimit  = tokenLimit;
     this.description = description;
+    this.assignees = assignees;
     
     this.id          = id;
     this.created_at  = created_at;
